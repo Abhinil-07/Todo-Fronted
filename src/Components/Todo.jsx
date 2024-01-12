2910ff7111bc775efa94d10e57b2842725794781
Abhinil-07 <abhinilnath10@gmail.com>
2024-01-12 10:30:21 +0530

    Added delete functionality'

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,35 +1,68 @@
 import axios from "axios";
 // import "./index.css"
-const Todo = ({todos,fetchData}) => {
-
-    const containerStyle = {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 1fr)',
-        gap: '16px', 
-       
-      };
+const Todo = ({ todos, fetchData }) => {
+  const containerStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 1fr)",
+    gap: "16px",
+  };
   return (
     <div style={containerStyle}>
-    {todos.map(function (todo,index) {
-    return (
-        <div style={{ border: '1px solid white',padding:2, marginTop:4}} key={todo._id}>
-            <h2>{index+1}. {todo.title}</h2>
+      {todos.map(function (todo, index) {
+        return (
+          <div
+            style={{ border: "1px solid white", padding: 2, marginTop: 4 }}
+            key={todo._id}
+          >
+            <h2>
+              {index + 1}. {todo.title}
+            </h2>
             <p>{todo.description}</p>
-            <p style={{padding:2}}>Status:{todo.completed === false ? "Pendng" : "Completed"}</p>
-            <button onClick={function(){
-                console.log(todo._id)
-                axios.put("https://todo-backend-lac-omega.vercel.app/edit-todos", {
-                   id: todo._id
+            <p style={{ padding: 2 }}>
+              Status:{todo.completed === false ? "Pendng" : "Completed"}
+            </p>
+            <button
+              onClick={async function () {
+                console.log(todo._id);
+                await axios.put(
+                  "https://todo-backend-lac-omega.vercel.app/edit-todos",
+                  {
+                    id: todo._id,
+                  }
+                );
+                alert("Updated");
+                fetchData();
+              }}
+            >
+              {todo.completed === true ? "Completed" : "Mark as completed"}
+            </button>
+            <button
+              onClick={async function () {
+                await axios
+                  .delete(
+                    `https://todo-backend-lac-omega.vercel.app/delete-todo/${todo._id}`
+                  )
+                  .then(() => {
+                    alert("Todo deleted successfully")
+                    fetchData();
+                  
+                  })
+                  .catch((error) => {
+                    console.error(
+                      "Delete failed:",
+                      error.response ? error.response.data : error.message
+                    );
+                    
                   });
-                  alert("Updated")
-                  fetchData();
-            }}>{todo.completed === true ? "Completed" : "Mark as completed"}</button>
-        </div>
-    );
-})}
-
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
